07c059a43f446cb10b185ac70f909be6ddc0ffca
Baptiste Wicht <baptiste.wicht@gmail.com>
2024-07-05 20:32:01 +0200

    Minor cleanups

--- include/FrontEnd.hpp
+++ include/FrontEnd.hpp
@@ -24,33 +24,34 @@ struct StringPool;
  * \class FrontEnd
  * \brief Represent the front end part of the compilation process. 
  */
-class FrontEnd {
-    public:
-        /*!
-         * Compile the given file into its MTAC Representation. 
-         * \param file The file that has to be compiled. 
-         * \return The MTAC Program representing the source program. 
-         */
-        virtual std::unique_ptr<mtac::Program> compile(const std::string& file, Platform platform) = 0;   
+struct FrontEnd {
+    /*!
+     * Compile the given file into its MTAC Representation. 
+     * \param file The file that has to be compiled. 
+     * \return The MTAC Program representing the source program. 
+     */
+    virtual std::unique_ptr<mtac::Program> compile(const std::string& file, Platform platform) = 0;   
 
-        /*!
-         * Set the string pool. 
-         * \param pool The string pool. 
-         */
-        void set_string_pool(std::shared_ptr<StringPool> pool);
-        
-        /*!
-         * Returns the string pool. 
-         * \return The string pool. 
-         */
-        std::shared_ptr<StringPool> get_string_pool();
+    virtual ~FrontEnd() = default;
 
-        void set_configuration(std::shared_ptr<Configuration> configuration);
-        std::shared_ptr<Configuration> get_configuration();
+    /*!
+     * Set the string pool. 
+     * \param pool The string pool. 
+     */
+    void set_string_pool(std::shared_ptr<StringPool> pool);
+    
+    /*!
+     * Returns the string pool. 
+     * \return The string pool. 
+     */
+    std::shared_ptr<StringPool> get_string_pool();
 
-    protected:
-        std::shared_ptr<StringPool> pool; /**< The string pool that is used during this compilation */
-        std::shared_ptr<Configuration> configuration; /**< The configuration of the compilation */
+    void set_configuration(std::shared_ptr<Configuration> configuration);
+    std::shared_ptr<Configuration> get_configuration();
+
+protected:
+    std::shared_ptr<StringPool> pool; /**< The string pool that is used during this compilation */
+    std::shared_ptr<Configuration> configuration; /**< The configuration of the compilation */
 };
 
 }
